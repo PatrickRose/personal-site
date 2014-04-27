5d83c1289f37c870d13f6bee08074255486a5853
Patrick Rose <pjr0911025@googlemail.com>
2014-04-27 14:26:12 +0100

    Use partials for blog posts

--- app/views/blog/index.blade.php
+++ app/views/blog/index.blade.php
@@ -8,16 +8,7 @@
    @foreach(array_chunk($blogs->all(), 3) as $row)
        <div class="row">
            @foreach($row as $blog)
-               <div class="col-md-4">
-                   <div class="blog-title">
-                       <h2>{{ link_to_route("blog.show", ucwords($blog->title), $blog->slug) }}</h2>
-                   </div>
-                   <div class="blog-text">
-                       {{ $blog->getFirstParagraph() }}
-                   </div>
-                   {{ link_to_route("blog.show", "Continue Reading...",
-                   $blog->slug, array('class' => 'btn btn-blog btn-block')) }}
-               </div>
+               @include('partials/_blog')
            @endforeach
        </div>
    @endforeach
---
+++ app/views/partials/_blog.blade.php
@@ -0,0 +1,10 @@
+<div class="col-md-4">
+    <div class="blog-title">
+        <h2>{{ link_to_route("blog.show", ucwords($blog->title), $blog->slug) }}</h2>
+    </div>
+    <div class="blog-text">
+        {{ $blog->getFirstParagraph() }}
+    </div>
+    {{ link_to_route("blog.show", "Continue Reading...",
+    $blog->slug, array('class' => 'btn btn-blog btn-block')) }}
+</div>
