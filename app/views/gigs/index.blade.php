c78dfa9a47441b9be09199b6c3083ffd68d49d4b
Patrick Rose <pjr0911025@googlemail.com>
2015-09-13 22:29:52 +0100

    Don't show ticket link if there isn't one

--- app/views/gigs/index.blade.php
+++ app/views/gigs/index.blade.php
@@ -5,70 +5,74 @@
 @stop
 
 @section('content')
-<div class="page-header">
-    <h1>
-        Upcoming Gigs
-    </h1>
-</div>
+    <div class="page-header">
+        <h1>
+            Upcoming Gigs
+        </h1>
+    </div>
 
-<div class="table-responsive">
-    <table class="table gig-table">
-        <thead>
-        <tr>
-            <th>
-                Date
-            </th>
-            <th>
-                Time
-            </th>
-            <th>
-                Location
-            </th>
-            <th>
-                About
-            </th>
-            <th>
-                Cost
-            </th>
-            <th>
-                Ticket Link
-            </th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($gigs as $gig)
-            <tr>
-                <td>
-                    {{ $gig->date }}
-                </td>
-                <td>
-                    {{ $gig->time }}
-                </td>
-                <td>
-                    {{ $gig->location }}
-                </td>
-                <td>
-                    {{ $gig->about }}
-                </td>
-                <td>
-                    {{ $gig->cost }}
-                </td>
-                <td>
-                    <a href="{{ $gig->ticketlink }}">
+    <div class="table-responsive">
+        <table class="table gig-table">
+            <thead>
+                <tr>
+                    <th>
+                        Date
+                    </th>
+                    <th>
+                        Time
+                    </th>
+                    <th>
+                        Location
+                    </th>
+                    <th>
+                        About
+                    </th>
+                    <th>
+                        Cost
+                    </th>
+                    <th>
                         Ticket Link
-                    </a>
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($gigs as $gig)
+                    <tr>
+                        <td>
+                            {{ $gig->date }}
+                        </td>
+                        <td>
+                            {{ $gig->time }}
+                        </td>
+                        <td>
+                            {{ $gig->location }}
+                        </td>
+                        <td>
+                            {{ $gig->about }}
+                        </td>
+                        <td>
+                            {{ $gig->cost }}
+                        </td>
+                        <td>
+                            @if ($gig->ticketLink)
+                                <a href="{{ $gig->ticketlink }}">
+                                    Ticket Link
+                                </a>
+			    @else
+				No ticket link
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
 
-    @if($gigs->isEmpty())
-        <div class="alert alert-info">
-            No bookings, check back soon!
-        </div>
-    @endif
+        @if($gigs->isEmpty())
+            <div class="alert alert-info">
+                No bookings, check back soon!
+            </div>
+        @endif
 
-    {{ $gigs->links() }}
-</div>
+        {{ $gigs->links() }}
+    </div>
 @stop
