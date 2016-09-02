2c74ba1c4f4746d85d710fee5b12a5bf4e37e579
Patrick Rose <pjr0911025@googlemail.com>
2016-09-02 23:16:32 +0100

    Use database on home page...

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,14 +3,28 @@
 namespace PatrickRose\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use PatrickRose\Repositories\BlogRepositoryInterface;
+use PatrickRose\Repositories\GigRepositoryInterface;
 use PatrickRose\Http\Requests;
 
 class StaticPagesController extends Controller
 {
+    /**
+     * @var PatrickRose\Repositories\BlogRepositoryInterface
+     */
+    private $blogRepo;
+
+    private $gigRepo;
+
+    function __construct(BlogRepositoryInterface $blogRepo, GigRepositoryInterface $gigRepo) {
+        $this->blogRepo = $blogRepo;
+        $this->gigRepo = $gigRepo;
+    }
+    
     public function homePage()
     {
-        return \View::make("staticpages.home", ['blogs' => []]);
+        $blogs = $this->blogRepo->getOnly(4);
+        return \View::make("staticpages.home", compact('blogs'))
     }
 
     public function aboutPage()
@@ -20,11 +34,14 @@ class StaticPagesController extends Controller
 
     public function gigPage()
     {
-        return \View::make('staticpages.gigs', []);
+        $gigs = $this->gigRepo->all();
+        return \View::make('staticpages.gigs', compact('gigs'));
     }
 
     public function feedPage()
     {
+        $blogs = $this->blogRepo->all();
+        
         return \Response::view('staticpages.rss', compact("blogs"), 200)->header("Content-Type", "application/atom+xml; charset=UTF-8");
     }
 }
