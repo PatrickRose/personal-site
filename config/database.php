9b1eb2316bca27ac10da65b25e28ed58f0afc8fa
Patrick Rose <pjr0911025@googlemail.com>
2016-09-02 23:00:58 +0100

    Remove prefix

--- config/database.php
+++ config/database.php
@@ -61,7 +61,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
-            'prefix' => 'personal_',
+            'prefix' => env('DB_PREFIX', ''),
             'strict' => true,
             'engine' => null,
         ],
